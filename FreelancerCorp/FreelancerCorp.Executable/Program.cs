8ecd44530b9a3d7551307943f3bf708ceed31ee6
Nikola Kunzová <nikola.kunzova@hotmail.sk>
2019-10-06 23:22:24 +0200

    Update Program.cs
    Added new objects to the database

--- FreelancerCorp/FreelancerCorp.Executable/Program.cs
+++ FreelancerCorp/FreelancerCorp.Executable/Program.cs
@@ -8,7 +8,12 @@ namespace FreelancerCorp.Executable {
 
             using (var db = new FreelancerCorpDbContext()) {
                 db.FreeInfos.Add(new FreelancerInfo("Imre Gejza", "Brno", "noob@gmail.com", new DateTime(2002, 2, 2), Enums.Sex.FEMALE));
-                //db.Freelancers.Add(new Freelancer());
+                db.CorpInfos.Add(new CorporationInfo("Punch Job", "Brno", "letmepunchyou@gmail.com"));
+
+                db.SaveChanges();
+
+                db.Freelancers.Add(new Freelancer(1));
+                db.Corporations.Add(new Corporation(1));
 
                 db.SaveChanges();
             }
