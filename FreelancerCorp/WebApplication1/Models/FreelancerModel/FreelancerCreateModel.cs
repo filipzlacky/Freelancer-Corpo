c7e67983cbe2deb128182404ff3d37b4e9ce916e
Nikola Kunzová <nikola.kunzova@hotmail.sk>
2019-12-07 22:33:51 +0100

    Deleted WebApplication1 and .vs

--- FreelancerCorp/WebApplication1/Models/FreelancerModel/FreelancerCreateModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using FreelancerCorp.BusinessLayer.DTOs;
-
-namespace FreelancerCorp.WebApi.Models.FreelancerModel
-{
-    public class FreelancerCreateModel
-    {
-        public FreelancerDTO FreelancerDto { get; set; }
-    }
-}
